2fa4532a1ce66caf89696014b73bf103e09fc0c7
byeol <qufgml0216@naver.com>
2022-10-19 00:48:32 +0900

    [Add] KoElectra 모델 추가

--- models/model.py
+++ models/model.py
@@ -1,6 +1,7 @@
 import torch
 import torch.nn as nn
 from transformers import XLMRobertaModel
+from transformers import ElectraModel, ElectraTokenizer
 
 
 class SimpleClassifier(nn.Module):
@@ -29,7 +30,7 @@ class RoBertaBaseClassifier(nn.Module):
         super(RoBertaBaseClassifier, self).__init__()
 
         self.num_label = num_label
-        self.xlm_roberta = XLMRobertaModel.from_pretrained(config.model)
+        self.xlm_roberta = XLMRobertaModel.from_pretrained("xlm-roberta-base")
         self.xlm_roberta.resize_token_embeddings(
             len_tokenizer
         )  # len_tokenizer = 250010
@@ -38,6 +39,33 @@ class RoBertaBaseClassifier(nn.Module):
 
     def forward(self, input_ids, attention_mask, labels=None):
         outputs = self.xlm_roberta(
+            input_ids=input_ids, attention_mask=attention_mask, token_type_ids=None
+        )
+        sequence_output = outputs[0]
+        logits = self.labels_classifier(sequence_output)
+
+        loss = None
+
+        if labels is not None:
+            loss_fct = nn.CrossEntropyLoss()
+            loss = loss_fct(logits.view(-1, self.num_label), labels.view(-1))
+
+        return loss, logits
+
+
+class KoElectraClassifier(nn.Module):
+    def __init__(self, config, num_label, len_tokenizer):
+        super(KoElectraClassifier, self).__init__()
+
+        self.num_label = num_label
+        self.koelectra = ElectraModel.from_pretrained(
+            "monologg/koelectra-base-v3-discriminator"
+        )
+        self.koelectra.resize_token_embeddings(len_tokenizer)
+        self.labels_classifier = SimpleClassifier(config, self.num_label)
+
+    def forward(self, input_ids, attention_mask, labels=None):
+        outputs = self.koelectra(
             input_ids=input_ids, attention_mask=attention_mask, token_type_ids=None
         )
         sequence_output = outputs[0]
