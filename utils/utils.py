05d27af97d5357b363c43f3787ca57fb1d132740
byeol <qufgml0216@naver.com>
2022-10-20 01:39:39 +0900

    [Add] labels to id 및 id to labels 을 json 형식으로 따로 저장하고 불러올 수 있도록 함

--- utils/utils.py
+++ utils/utils.py
@@ -1,11 +1,27 @@
 import json
 
 
+def get_labels(f_path="./label.json"):
+    """label_to_id 및 label_name_to_id를 json 파일로부터 불러옴
+
+    Args:
+        f_path (str): label.json 경로 Defaults to "./label.json".
+
+    Returns:
+        label 정보가 담겨있는 json
+    """
+
+    with open(f_path, "r") as f:
+        labels = json.load(f)
+        
+    return labels
+
+
 def jsonload(fname: str, encoding="utf-8"):
     """config.json 파일을 로드
 
     Args:
-        fname (str): _description_
+        fname (str): 파일명
         encoding (str): Defaults to "utf-8".
 
     Returns:
@@ -21,7 +37,7 @@ def jsonlload(fname: str, encoding="utf-8"):
     """jsonl 데이터 파일을 읽어서 리스트로 반환
 
     Args:
-        fname (str): _description_
+        fname (str):파일명
         encoding (str): Defaults to "utf-8".
 
     Returns:
